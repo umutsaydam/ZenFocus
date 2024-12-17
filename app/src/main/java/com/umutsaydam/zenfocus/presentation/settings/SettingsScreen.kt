6b60d86d450ce59b4c7d446214b51d26121eea84
umutsaydam <umutsaydam24@gmail.com>
2024-12-17 08:47:11 +0300

    Edit: removed unused libraries in MenuItemSwitch, SettingsScreen

--- app/src/main/java/com/umutsaydam/zenfocus/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/umutsaydam/zenfocus/presentation/settings/SettingsScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
