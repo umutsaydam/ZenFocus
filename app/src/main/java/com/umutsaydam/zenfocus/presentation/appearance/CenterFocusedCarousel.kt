401e52a9a91bbf8c90ef5a2c73b249c42a1ef6c5
umutsaydam <umutsaydam24@gmail.com>
2024-12-17 14:16:35 +0300

    Add|Edit: CenterFocusedCarousel, gradient added and borders removed

--- app/src/main/java/com/umutsaydam/zenfocus/presentation/appearance/CenterFocusedCarousel.kt
+++ app/src/main/java/com/umutsaydam/zenfocus/presentation/appearance/CenterFocusedCarousel.kt
@@ -1,13 +1,14 @@
 package com.umutsaydam.zenfocus.presentation.appearance
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -15,6 +16,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -37,22 +39,46 @@ fun CenterFocusedCarousel(
         }
     }
 
-    Text(text = "First Visible Index: $visibleIndex")
-
-    LazyHorizontalGrid(
-        state = gridState,
+    Box(
         modifier = modifier
             .width(300.dp)
-            .background(Color.Yellow),
-        rows = GridCells.Fixed(1),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(
-            horizontal = 16.dp,
-            vertical = 8.dp
-            )
     ) {
-        items(listOfTheme.size) { currentIndex ->
-            content(visibleIndex, currentIndex)
+        LazyHorizontalGrid(
+            state = gridState,
+            modifier = modifier
+                .width(300.dp),
+            rows = GridCells.Fixed(1),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(
+                horizontal = 16.dp,
+                vertical = 12.dp
+            )
+        ) {
+            items(listOfTheme.size) { currentIndex ->
+                content(visibleIndex, currentIndex)
+            }
         }
+
+        HorizontalLinearLinearGradient(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(35.dp)
+                .align(Alignment.CenterStart),
+            colorList = listOf(
+                MaterialTheme.colorScheme.surfaceContainerLow,
+                Color.Transparent
+            )
+        )
+
+        HorizontalLinearLinearGradient(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(35.dp)
+                .align(Alignment.CenterEnd),
+            colorList = listOf(
+                Color.Transparent,
+                MaterialTheme.colorScheme.surfaceContainerLow
+            )
+        )
     }
 }
