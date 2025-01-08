91e0171e5770c44158b66ea03cbfbaea190007b4
umutsaydam <umutsaydam24@gmail.com>
2025-01-08 11:39:39 +0300

    Edit: version upgraded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.umutsaydam.zenfocus"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         val properties: Properties = Properties()
         properties.load(
