e5b35850e7316c1f0dde7e630b9c8ef8c5bd86a7
umutsaydam <umutsaydam24@gmail.com>
2025-03-29 20:09:05 +0300

    VersionCode upgrade 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.umutsaydam.zenfocus"
         minSdk = 24
         targetSdk = 35
-        versionCode = 33
-        versionName = "1.0.3"
+        versionCode = 34
+        versionName = "1.0.4"
 
         val properties: Properties = Properties()
         properties.load(
